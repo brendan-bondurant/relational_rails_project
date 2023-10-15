9b0b3ac0af93f568e6a1587119bbf0b32988705d
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-10-15 09:11:11 -0600

    Update: Minimal tests setup and  passing

--- app/controllers/instrument_builders_controller.rb
+++ app/controllers/instrument_builders_controller.rb
@@ -7,4 +7,8 @@ class InstrumentBuildersController < ApplicationController
     # require 'pry'; binding.pry
     @instrument_builder = InstrumentBuilder.find(params[:id])
   end
+
+  def new
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/instrument_builders", to: "instrument_builders#index", as: "builders" 
+  get "/instrument_builders/new", to: "instrument_builders#new"
   get "/instrument_builders/:id", to: "instrument_builders#show"
   get "/models", to: "models#index", as: "models"
   get "/models/:id", to: "models#show"
